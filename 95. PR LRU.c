90c6715a7a930e7fd2eff984d00b4c3aef24fd88
vijethmd <vijethmd.cs23@bmsce.ac.in>
2025-05-21 08:56:22 +0530

    Update 95. PR LRU.c

--- 95. PR LRU.c
+++ 95. PR LRU.c
@@ -1,1 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int search(int key, int frame[], int frameSize) {
+    for (int i = 0; i < frameSize; i++) {
+        if (frame[i] == key)
+            return i;
+    }
+    return -1;
+}
+
+void simulateLRU(int pages[], int n, int frameSize) {
+    int frame[frameSize], time[frameSize], count = 0, hits = 0;
+
+    for (int i = 0; i < frameSize; i++) {
+        frame[i] = -1;
+        time[i] = 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        int pos = search(pages[i], frame, frameSize);
+        if (pos == -1) {
+            int least = 0;
+            for (int j = 1; j < frameSize; j++) {
+                if (time[j] < time[least])
+                    least = j;
+            }
+            frame[least] = pages[i];
+            time[least] = i;
+            count++;
+        } else {
+            hits++;
+            time[pos] = i;
+        }
+    }
+    printf("LRU Page Faults: %d, Page Hits: %d\n", count, hits);
+}
+
+
+int main() {
+    int n, frameSize;
+    printf("Enter the size of the pages: ");
+    scanf("%d", &n);
+
+    int pages[n];
+    printf("Enter the page strings: ");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &pages[i]);
+
+    printf("Enter the no of page frames: ");
+    scanf("%d", &frameSize);
+
+    simulateFIFO(pages, n, frameSize);
+
+
+    return 0;
+}
