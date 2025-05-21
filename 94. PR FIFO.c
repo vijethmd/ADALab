51a31e8c317a7066b18bd172c8003c5b96836b7a
vijethmd <vijethmd.cs23@bmsce.ac.in>
2025-05-21 08:49:53 +0530

    Update 94. PR FIFO.c

--- 94. PR FIFO.c
+++ 94. PR FIFO.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int i, j, n, capacity, pageFaults = 0, idx = 0;
+    int i, j, n, capacity, pageFaults = 0, pageHits = 0, idx = 0;
 
     printf("Enter number of pages: ");
     scanf("%d", &n);
@@ -20,9 +20,13 @@ int main() {
         frames[i] = -1;  // initialize frames as empty
     }
 
+    printf("\nPage\tFrame Status\t\tResult\n");
+    printf("-------------------------------------------------\n");
+
     for (i = 0; i < n; i++) {
         int found = 0;
-        // Check if page is already in frame (hit)
+
+        // Check for page hit
         for (j = 0; j < capacity; j++) {
             if (frames[j] == pages[i]) {
                 found = 1;
@@ -30,25 +34,37 @@ int main() {
             }
         }
 
-        if (!found) {
-            // Page fault occurs
-            frames[idx] = pages[i];
-            idx = (idx + 1) % capacity;  // FIFO index rotation
-            pageFaults++;
+        printf("%d\t", pages[i]);
 
-            // Print frames status after this replacement
-            printf("Page %d -> Frames: ", pages[i]);
+        if (found) {
+            pageHits++;
+            // Print frames
             for (j = 0; j < capacity; j++) {
                 if (frames[j] != -1)
                     printf("%d ", frames[j]);
                 else
                     printf("- ");
             }
-            printf("\n");
+            printf("\t\tHIT\n");
+        } else {
+            // Page fault
+            frames[idx] = pages[i];
+            idx = (idx + 1) % capacity;
+            pageFaults++;
+
+            // Print frames
+            for (j = 0; j < capacity; j++) {
+                if (frames[j] != -1)
+                    printf("%d ", frames[j]);
+                else
+                    printf("- ");
+            }
+            printf("\t\tFAULT\n");
         }
     }
 
     printf("\nTotal Page Faults: %d\n", pageFaults);
+    printf("Total Page Hits  : %d\n", pageHits);
 
     return 0;
 }
